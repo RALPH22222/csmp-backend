8cd5d8ff1c6a4470220b234df99a03d5a232e156
PajiiMarr <151644959+PajiiMarr@users.noreply.github.com>
2026-07-09 20:31:53 +0800

    config: php token issuer added

--- config/stellar.js
+++ config/stellar.js
@@ -9,4 +9,5 @@ export const networkPassphrase = process.env.NETWORK_PASSPHRASE || 'Test SDF Fut
 export const PHP_TOKEN_ADDRESS = process.env.PHP_TOKEN_ADDRESS;
 export const PHP_TOKEN_CODE = process.env.PHP_TOKEN_CODE;
 export const PHP_TOKEN_ISSUER = process.env.PHP_TOKEN_ISSUER;
+export const PHP_TOKEN_ISSUER_SECRET = process.env.PHP_TOKEN_ISSUER_SECRET;
 export const PHP_DECIMALS = 7;
